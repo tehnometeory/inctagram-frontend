6b56e209aaf250ec0e6f180dc09db8f9af38b678
Dmitriy <goglev37@gmail.com>
2024-11-16 20:35:02 +0300

    PROJ_41 feat: add basic form with styles

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,3 +1,34 @@
+'use client'
+import { useForm } from 'react-hook-form'
+
+import { ControlledInput } from '@/shared/ui'
+import { Button, Card, ReCaptcha } from '@rambo-react/ui-meteors'
+import Link from 'next/link'
+
+import s from './ForgotPassword.module.scss'
+
 export default function ForgotPassword() {
-  return <div>Forgot Password</div>
+  const { control } = useForm()
+
+  return (
+    <Card>
+      <h1 className={s.title}>Forgot Password</h1>
+      <form>
+        <ControlledInput
+          control={control}
+          label={'Email'}
+          name={'email'}
+          placeholder={'Epam@epam.com'}
+        />
+        <p className={s.text}>
+          Enter your email address and we will send you further instructions{' '}
+        </p>
+        <Button fullWidth>Send Link</Button>
+      </form>
+      <Link className={s.signIn} href={'sign-in'}>
+        Back to Sign In
+      </Link>
+      <ReCaptcha onChange={() => {}} siteKey={'www'} theme={'light'} />
+    </Card>
+  )
 }
