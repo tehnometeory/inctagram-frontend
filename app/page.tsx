4a024a081dc4f478d4c7bfebfa756ad8e12f4f9f
Yan Turnt <bx6xbchosen1@gmail.com>
2024-11-17 17:30:57 +0500

    PROJ-45_store fix: move store provider to the root layout file to work correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-'use client'
-
-import { Provider } from 'react-redux'
-
-import { store } from './store'
-
 export default function Home() {
-  return (
-    <Provider store={store}>
-      <div>Inctagram</div>
-    </Provider>
-  )
+  return <div>Inctagram</div>
 }
