a5a61e2f6881612777c790256796a70681d18925
Dmitriy <goglev37@gmail.com>
2024-11-18 01:02:49 +0300

    PROJ_41 feat: add modal

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,14 +1,25 @@
 'use client'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { ControlledInput } from '@/shared/ui'
-import { Button, Card, ReCaptcha } from '@rambo-react/ui-meteors'
+import { Button, Card, Modal, ReCaptcha } from '@rambo-react/ui-meteors'
 import Link from 'next/link'
 
 import s from './ForgotPassword.module.scss'
 
 export default function ForgotPassword() {
-  const { control } = useForm()
+  const { control, reset, setError } = useForm()
+  const [showModal, setShowModal] = useState(true)
+  const [email, setEmail] = useState(null)
+  const handleCloseShowModal = () => {
+    setShowModal(false)
+    reset()
+  }
+
+  // if (isError) {
+  //   setError('email', { message: "User with this email doesn't exist", type: 'manual' })
+  // }
 
   return (
     <Card>
@@ -18,7 +29,7 @@ export default function ForgotPassword() {
           control={control}
           label={'Email'}
           name={'email'}
-          placeholder={'Epam@epam.com'}
+          placeholder={email ?? 'Epam@epam.com'}
         />
         <p className={s.text}>
           Enter your email address and we will send you further instructions{' '}
@@ -29,6 +40,14 @@ export default function ForgotPassword() {
         Back to Sign In
       </Link>
       <ReCaptcha onChange={() => {}} siteKey={'www'} theme={'light'} />
+      <Modal isOpen={showModal} onClose={handleCloseShowModal} title={'Email sent'}>
+        <p className={s.textModal}>We have sent a link to confirm your email to {email}</p>
+        <div className={s.btnWrapper}>
+          <Button className={s.btnModal} onClick={handleCloseShowModal}>
+            OK
+          </Button>
+        </div>
+      </Modal>
     </Card>
   )
 }
