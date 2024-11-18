31c22a58662757772abbbf29966b1f85298bf9ab
Dmitriy <goglev37@gmail.com>
2024-11-18 19:28:50 +0300

    PROJ_41 feat: Recaptcha

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,17 +1,22 @@
 'use client'
 import { useState } from 'react'
+import { useGoogleReCaptcha } from 'react-google-recaptcha-v3'
 import { useForm } from 'react-hook-form'
 
 import { ControlledInput } from '@/shared/ui'
-import { Button, Card, Modal, ReCaptcha } from '@rambo-react/ui-meteors'
+import { Button, Card, Modal, Recaptcha } from '@rambo-react/ui-meteors'
 import Link from 'next/link'
 
 import s from './ForgotPassword.module.scss'
 
 export default function ForgotPassword() {
   const { control, reset, setError } = useForm()
-  const [showModal, setShowModal] = useState(true)
+  const [showModal, setShowModal] = useState(false)
+  const [reCaptcha, setReCaptcha] = useState<
+    'checked' | 'expired' | 'initial' | 'loading' | 'withError'
+  >('initial')
   const [email, setEmail] = useState(null)
+  const { executeRecaptcha } = useGoogleReCaptcha()
   const handleCloseShowModal = () => {
     setShowModal(false)
     reset()
@@ -39,7 +44,7 @@ export default function ForgotPassword() {
       <Link className={s.signIn} href={'sign-in'}>
         Back to Sign In
       </Link>
-      <ReCaptcha onChange={() => {}} siteKey={'www'} theme={'light'} />
+      <Recaptcha label={'I’m not a robot'} variant={reCaptcha} />
       <Modal isOpen={showModal} onClose={handleCloseShowModal} title={'Email sent'}>
         <p className={s.textModal}>We have sent a link to confirm your email to {email}</p>
         <div className={s.btnWrapper}>
