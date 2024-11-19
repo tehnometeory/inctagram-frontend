5d30c8e3665b99d01c964f61358480eff6b8acb9
Dmitriy <goglev37@gmail.com>
2024-11-19 13:53:25 +0300

    PROJ_41 feat: add handleSubmitReCaptchaForm function for handling reCAPTCHA form submission

--- features/ForgotPassword/ui/ForgotPassword.tsx
+++ features/ForgotPassword/ui/ForgotPassword.tsx
@@ -18,11 +18,15 @@ export function ForgotPasswordRecaptchaWrapper() {
 }
 
 function ForgotPassword() {
-  const { control, reset, setError } = useForm()
+  const { control, handleSubmit, reset, setError } = useForm()
   const [showModal, setShowModal] = useState(false)
+  const [reCaptchaToken, setReCaptchaToken] = useState('')
   const [reCaptcha, setReCaptcha] = useState<
     'checked' | 'expired' | 'initial' | 'loading' | 'withError'
   >('initial')
+
+  const [sendLinkStatus, setSendLinkStatus] = useState<'initial' | 'success'>('initial')
+
   const [email, setEmail] = useState(null)
   const { executeRecaptcha } = useGoogleReCaptcha()
   const handleCloseShowModal = () => {
@@ -30,9 +34,28 @@ function ForgotPassword() {
     reset()
   }
 
-  // if (isError) {
-  //   setError('email', { message: "User with this email doesn't exist", type: 'manual' })
-  // }
+  const handleSubmitReCaptchaForm = async (event: any) => {
+    event.preventDefault()
+    setReCaptcha('loading')
+    if (!executeRecaptcha) {
+      setError('email', { message: 'Execute recaptcha not yet available', type: 'manual' })
+
+      return
+    }
+
+    const token = await executeRecaptcha('password_recovery')
+
+    setReCaptchaToken(token)
+    setReCaptcha('checked')
+  }
+
+  const isDisabled = () => {
+    if (sendLinkStatus === 'success') {
+      return false
+    }
+
+    return !reCaptchaToken
+  }
 
   return (
     <section className={s.cardWrapper}>
@@ -48,14 +71,29 @@ function ForgotPassword() {
           <p className={s.text}>
             Enter your email address and we will send you further instructions
           </p>
-          <Button fullWidth>Send Link</Button>
+          {sendLinkStatus === 'success' && (
+            <p className={s.textSuccess}>
+              The link has been sent by email. If you don’t receive an email send link again
+            </p>
+          )}
+          <Button
+            disabled={isDisabled()}
+            fullWidth
+            type={sendLinkStatus === 'success' ? 'button' : 'submit'}
+            variant={'primary'}
+          >
+            {sendLinkStatus === 'success' ? 'Send Link Again' : 'Send Link'}
+          </Button>
         </form>
         <Link className={s.signIn} href={'sign-in'}>
           Back to Sign In
         </Link>
-        <form className={s.reCaptcha}>
-          <Recaptcha label={'I’m not a robot'} variant={reCaptcha} />
-        </form>
+
+        {sendLinkStatus !== 'success' && (
+          <form className={s.reCaptcha} onSubmit={handleSubmitReCaptchaForm}>
+            <Recaptcha label={'I’m not a robot'} variant={reCaptcha} />
+          </form>
+        )}
         <Modal isOpen={showModal} onClose={handleCloseShowModal} title={'Email sent'}>
           <p className={s.textModal}>We have sent a link to confirm your email to {email}</p>
           <div className={s.btnWrapper}>
