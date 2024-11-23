4b34ee66c563468377054f2d05b1c40c83c6fd74
Mary-No <mascha.novik@gmail.com>
2024-11-23 17:21:03 +0100

    PROJ-43 fix: fix ConfirmedEmailPage

--- app/auth/confirmed-email/page.tsx
+++ app/auth/confirmed-email/page.tsx
@@ -1,8 +1,11 @@
 'use client'
-
-import { EmailConfirmationWrapper, ImgSettings } from '@/shared/ui/EmailConfirmationWrapper'
 import { Button } from '@rambo-react/ui-meteors'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+
+import styles from './ConfirmedEmail.module.scss'
+
+import ConfirmedEmailImg from '../../../public/images/sign-up.svg'
 
 export default function ConfirmedEmail() {
   const router = useRouter()
@@ -10,22 +13,26 @@ export default function ConfirmedEmail() {
   const handleSignIn = () => {
     router.push('/auth/sign-in')
   }
-  const imgSettings: ImgSettings = {
-    desktop: { height: 300, width: 430 },
-    mobile: { height: 220, width: 320 },
-    src: '/images/sign-up.svg',
-    tablet: { height: 250, width: 350 },
-  }
 
   return (
-    <EmailConfirmationWrapper
-      description={'Your email has been confirmed'}
-      imgSettings={imgSettings}
-      title={'Congratulations!'}
-    >
+    <div className={styles.container}>
+      <h1 className={styles.title}>Congratulations!</h1>
+      <p className={styles.description}>Your email has been confirmed</p>
       <Button fullWidth={false} onClick={handleSignIn} variant={'primary'}>
         Sign In
       </Button>
-    </EmailConfirmationWrapper>
+      <Image
+        alt={'confirmed email'}
+        className={styles.img}
+        height={300}
+        sizes={`(max-width: 425px) 320px,
+                432px`}
+        src={ConfirmedEmailImg}
+        style={{
+          width: '100%',
+        }}
+        width={432}
+      />
+    </div>
   )
 }
