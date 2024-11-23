48f5f4909242d7a53f9879c80aa401542f6affe5
Dmitriy <goglev37@gmail.com>
2024-11-23 15:25:09 +0300

    PROJ-41_refactor: refactor styles

--- features/ForgotPassword/ui/ForgotPassword.tsx
+++ features/ForgotPassword/ui/ForgotPassword.tsx
@@ -3,14 +3,10 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 import { FormForgotPassword } from '@/features/ForgotPassword/ui/FormForgotPassword'
 
-import s from './ForgotPassword.module.scss'
-
 export function ForgotPassword() {
   return (
     <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_PUBLIC_KEY as string}>
-      <section className={s.cardWrapper}>
-        <FormForgotPassword />
-      </section>
+      <FormForgotPassword />
     </GoogleReCaptchaProvider>
   )
 }
