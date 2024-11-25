31c988905529257657069c96314fd14cee076fbd
Mary-No <mascha.novik@gmail.com>
2024-11-25 11:52:33 +0100

    PROJ-43 fix: fix styles

--- app/auth/confirmed-email/page.tsx
+++ app/auth/confirmed-email/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import { Button } from '@rambo-react/ui-meteors'
 import { jwtDecode } from 'jwt-decode'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import styles from './ConfirmedEmail.module.scss'
@@ -17,40 +18,40 @@ type JWTPayload = {
 export default function ConfirmedEmailPage() {
   const router = useRouter()
   const searchParams = useSearchParams()
-
-  useEffect(() => {
-    const token = searchParams.get('code')
-
-    if (!token) {
-      router.replace('/auth/expired-email')
-
-      return
-    }
-    try {
-      const { exp }: JWTPayload = jwtDecode(token)
-      const now = Math.floor(Date.now() / 1000)
-
-      if (now > exp) {
-        router.replace('/auth/expired-email')
-
-        return
-      }
-    } catch (error) {
-      console.error('token processing error: ', error)
-      router.replace('/auth/expired-email')
-    }
-  }, [searchParams, router])
-  const handleSignIn = () => {
-    router.push('/auth/sign-in')
-  }
+  //
+  // useEffect(() => {
+  //   const token = searchParams.get('code')
+  //
+  //   if (!token) {
+  //     router.replace('/auth/expired-email')
+  //
+  //     return
+  //   }
+  //   try {
+  //     const { exp }: JWTPayload = jwtDecode(token)
+  //     const now = Math.floor(Date.now() / 1000)
+  //
+  //     if (now > exp) {
+  //       router.replace('/auth/expired-email')
+  //
+  //       return
+  //     }
+  //   } catch (error) {
+  //     console.error('token processing error: ', error)
+  //     router.replace('/auth/expired-email')
+  //   }
+  // }, [searchParams, router])
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Congratulations!</h1>
       <p className={styles.description}>Your email has been confirmed</p>
-      <Button fullWidth={false} onClick={handleSignIn} variant={'primary'}>
-        Sign In
-      </Button>
+      <Link href={'/auth/sign-in'}>
+        <Button fullWidth={false} variant={'primary'}>
+          Sign In
+        </Button>
+      </Link>
+
       <Image
         alt={'confirmed email'}
         className={styles.img}
