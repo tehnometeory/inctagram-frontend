5d709e5bd484525fa06dd03bd92059972e6f0214
Mary-No <mascha.novik@gmail.com>
2024-11-25 20:10:10 +0100

    PROJ-43 refactor: change structure of the project

--- app/auth/confirmed-email/page.tsx
+++ app/auth/confirmed-email/page.tsx
@@ -2,14 +2,8 @@
 import { useEffect } from 'react'
 
 import { checkTokenValidity } from '@/shared/lib/CheckTokenValidity'
-import { Button } from '@rambo-react/ui-meteors'
-import Image from 'next/image'
-import Link from 'next/link'
+import { ConfirmedEmail } from '@/shared/ui/ConfirmedEmail/ConfirmedEmail'
 import { useRouter, useSearchParams } from 'next/navigation'
-
-import styles from './ConfirmedEmail.module.scss'
-
-import ConfirmedEmailImg from '../../../public/images/sign-up.svg'
 
 export default function ConfirmedEmailPage() {
   const router = useRouter()
@@ -30,28 +24,5 @@ export default function ConfirmedEmailPage() {
     }
   }, [searchParams, router])
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Congratulations!</h1>
-      <p className={styles.description}>Your email has been confirmed</p>
-      <Link className={styles.btnLink} href={'/auth/sign-in'}>
-        <Button fullWidth={false} variant={'primary'}>
-          Sign In
-        </Button>
-      </Link>
-
-      <Image
-        alt={'confirmed email'}
-        className={styles.img}
-        height={300}
-        sizes={`(max-width: 425px) 320px,
-                432px`}
-        src={ConfirmedEmailImg}
-        style={{
-          width: '100%',
-        }}
-        width={432}
-      />
-    </div>
-  )
+  return <ConfirmedEmail />
 }
