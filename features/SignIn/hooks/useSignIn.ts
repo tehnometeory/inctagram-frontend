8eb319f513204f5b80cf0113567648497cd09bff
krylov72 <qobject1337@gmail.com>
2024-11-26 14:28:00 +0500

    PROJ-42/sign-in init: create useSignIn hook

---
+++ features/SignIn/hooks/useSignIn.ts
@@ -0,0 +1,55 @@
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { useLazyLoginViaGitHubQuery, useLazyLoginViaGoogleQuery, useLoginMutation } from '../api'
+import { signInSchema } from '../model/validation'
+import { LoginErrorResponse } from './../api/types'
+
+export const useSignIn = () => {
+  const methods = useForm<FormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onBlur',
+    resolver: zodResolver(signInSchema),
+  })
+
+  type FormValues = z.infer<typeof signInSchema>
+
+  const [login, { isLoading }] = useLoginMutation()
+  const [triggerLoginViaGoogle] = useLazyLoginViaGoogleQuery()
+  const [triggerLoginViaGitHub] = useLazyLoginViaGitHubQuery()
+
+  const onSubmit = async (data: FormValues) => {
+    try {
+      const response = await login(data)
+
+      if (response.error) {
+        if ('data' in response.error) {
+          methods.setError('password', {
+            message: 'The email or password are incorrect. Try again please',
+          })
+        } else {
+          methods.setError('password', {
+            message: 'Check your internet connection',
+          })
+        }
+      }
+    } catch (e: any) {
+      methods.setError('password', {
+        message: 'Unknown error',
+      })
+    }
+  }
+
+  return {
+    isLoading,
+    methods,
+    onSubmit,
+    triggerLoginViaGitHub,
+    triggerLoginViaGoogle,
+  }
+}
