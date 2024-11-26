097cb13483b5aa43c96fa9ebdd12e0b602a2ee34
krylov72 <qobject1337@gmail.com>
2024-11-26 14:25:50 +0500

    PROJ-42/sign-in fix: add signInApi to store

--- app/store.ts
+++ app/store.ts
@@ -1,12 +1,17 @@
+import { signInApi } from '@/features/SignIn/api'
 import { signUpApi } from '@/features/SignUp/api/signUpApi'
 import { setPasswordApi } from '@/features/new-password/model/api'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(signUpApi.middleware).concat(setPasswordApi.middleware),
+    getDefaultMiddleware()
+      .concat(signUpApi.middleware)
+      .concat(setPasswordApi.middleware)
+      .concat(signInApi.middleware),
   reducer: {
     [setPasswordApi.reducerPath]: setPasswordApi.reducer,
+    [signInApi.reducerPath]: signInApi.reducer,
     [signUpApi.reducerPath]: signUpApi.reducer,
   },
 })
