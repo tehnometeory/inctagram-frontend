52ab282ac58a6b695b8fd3b17f6fddffbe38df7f
Mary-No <mascha.novik@gmail.com>
2024-11-27 17:15:03 +0100

    PROJ-43 refactor: remove useless code

--- shared/hooks/useCheckTokenValidity.ts
+++ shared/hooks/useCheckTokenValidity.ts
@@ -3,20 +3,20 @@ import { useEffect, useState } from 'react'
 import { checkTokenValidity } from '@/shared'
 import { useRouter, useSearchParams } from 'next/navigation'
 
-export const useCheckTokenValidity = (path: string): boolean => {
+export const useCheckTokenValidity = (redirectTo: string): boolean => {
+  const [isRedirecting, setIsRedirecting] = useState(true)
   const router = useRouter()
   const searchParams = useSearchParams()
-  const [isRedirecting, setIsRedirecting] = useState(true)
 
   const token = searchParams.get('code')
 
   useEffect(() => {
     if (!token || !checkTokenValidity(token)) {
-      router.replace(path)
+      router.replace(redirectTo)
     } else {
       setIsRedirecting(false)
     }
-  }, [token])
+  }, [token, redirectTo, router])
 
   return isRedirecting
 }
