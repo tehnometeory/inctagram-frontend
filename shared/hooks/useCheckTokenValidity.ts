a68be7200a5f67b80ed9dc10b6d9e32142e83fe4
Mary-No <mascha.novik@gmail.com>
2024-11-29 17:23:09 +0100

    PROJ-43 feat: add parameter to useCheckTokenValidity

--- shared/hooks/useCheckTokenValidity.ts
+++ shared/hooks/useCheckTokenValidity.ts
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react'
 import { checkTokenValidity } from '@/shared'
 import { useRouter, useSearchParams } from 'next/navigation'
 
-export const useCheckTokenValidity = (redirectTo: string): boolean => {
+export const useCheckTokenValidity = (redirectTo: string, searchParameter: string): boolean => {
   const [isRedirecting, setIsRedirecting] = useState(true)
   const router = useRouter()
   const searchParams = useSearchParams()
 
-  const token = searchParams.get('code')
+  const token = searchParams.get(searchParameter)
 
   useEffect(() => {
     if (!token || !checkTokenValidity(token)) {
