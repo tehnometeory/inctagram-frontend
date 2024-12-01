61b0c9f17b5604adade35c0af006076f861da503
krylov72 <qobject1337@gmail.com>
2024-12-01 18:03:08 +0500

    PROJ-42/sign-in refactor: remove unused imports

--- features/SignIn/api/types.ts
+++ features/SignIn/api/types.ts
@@ -2,19 +2,7 @@ export type LoginBody = {
   email: string
   password: string
 }
-export type LoginErrorResponse = {
-  errorsMessages: ErrorsMessages[]
-}
 
 export type LoginSuccessResponse = {
   accessToken: string
-}
-type ErrorsMessages = {
-  field: string
-  message: string
-}
-
-export type FormValues = {
-  email: string
-  password: string
 }
