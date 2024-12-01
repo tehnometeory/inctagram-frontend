a56db7f4a7c7fe1334fef22580eb6dea3c609a5f
krylov72 <qobject1337@gmail.com>
2024-12-01 18:02:52 +0500

    PROJ-42/sign-in refactor: refactor imports

--- features/SignIn/api/signInApi.ts
+++ features/SignIn/api/signInApi.ts
@@ -1,12 +1,12 @@
-import { BASE_URL_API } from '@/shared'
+import { BASE_URL_API, ErrorsMessagesResponse } from '@/shared'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { LoginBody, LoginErrorResponse, LoginSuccessResponse } from './types'
+import { LoginBody, LoginSuccessResponse } from './types'
 
 export const signInApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL_API }),
   endpoints: builder => ({
-    login: builder.mutation<LoginErrorResponse | LoginSuccessResponse, LoginBody>({
+    login: builder.mutation<ErrorsMessagesResponse | LoginSuccessResponse, LoginBody>({
       query: body => ({
         body,
         method: 'POST',
