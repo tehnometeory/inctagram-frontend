bbb0684b206568e1138eb211c3d5f5cef066df44
krylov72 <qobject1337@gmail.com>
2024-12-03 23:37:18 +0500

    PROJ-42/sign-in fix: fix useEffect dependence, fix name of styles

--- features/SignIn/ui/SignIn.tsx
+++ features/SignIn/ui/SignIn.tsx
@@ -26,69 +26,67 @@ export const SignIn = () => {
     if (token) {
       router.push('/home')
     }
-  })
+  }, [token, router])
 
   return (
-    <>
-      <FormProvider {...methods}>
-        <Card>
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <h1 className={styles.title}>Sign In</h1>
-            <div className={styles.socialContainer}>
-              <Google
-                className={styles.form__icon}
-                height={36}
-                onClick={() => {
-                  triggerLoginViaGoogle(undefined)
-                }}
-                width={36}
-              />
+    <FormProvider {...methods}>
+      <Card>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <h1 className={styles.title}>Sign In</h1>
+          <div className={styles.socialContainer}>
+            <Google
+              className={styles.formIcon}
+              height={36}
+              onClick={() => {
+                triggerLoginViaGoogle(undefined)
+              }}
+              width={36}
+            />
 
-              <Github
-                className={styles.form__icon}
-                fill={'white'}
-                height={36}
-                onClick={() => {
-                  triggerLoginViaGitHub(undefined)
-                }}
-                width={36}
-              />
-            </div>
+            <Github
+              className={styles.formIcon}
+              fill={'white'}
+              height={36}
+              onClick={() => {
+                triggerLoginViaGitHub(undefined)
+              }}
+              width={36}
+            />
+          </div>
 
-            <div className={styles.inputContainer}>
-              <ControlledInput
-                containerClassName={styles.emailInput}
-                control={methods.control}
-                label={'Email'}
-                name={'email'}
-                placeholder={'Epam@epam.com'}
-              />
-            </div>
-            <div className={styles.inputContainer}>
-              <ControlledInput
-                containerClassName={styles.passwordInput}
-                control={methods.control}
-                label={'Password'}
-                name={'password'}
-                placeholder={'*********'}
-                type={'password'}
-              />
-            </div>
-            <Link className={styles.forgotPass} href={'/auth/forgot-password'}>
-              Forgot Password
-            </Link>
-            <Button disabled={!methods.formState.isValid || isLoading} fullWidth type={'submit'}>
-              Sign In
+          <div className={styles.inputContainer}>
+            <ControlledInput
+              containerClassName={styles.emailInput}
+              control={methods.control}
+              label={'Email'}
+              name={'email'}
+              placeholder={'Epam@epam.com'}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <ControlledInput
+              containerClassName={styles.passwordInput}
+              control={methods.control}
+              label={'Password'}
+              name={'password'}
+              placeholder={'*********'}
+              type={'password'}
+            />
+          </div>
+          <Link className={styles.forgotPass} href={'/auth/forgot-password'}>
+            Forgot Password
+          </Link>
+          <Button disabled={!methods.formState.isValid || isLoading} fullWidth type={'submit'}>
+            Sign In
+          </Button>
+          <div className={styles.signIn__footer}>
+            <p className={styles.footer}>{`Don't have an account?`}</p>
+            <Button className={styles.signUpButton} fullWidth variant={'text'}>
+              <Link href={'/auth/sign-up'}>Sign Up</Link>
             </Button>
-            <div className={styles.signIn__footer}>
-              <p className={styles.footer}>{`Don't have an account?`}</p>
-              <Button className={styles.signUp_button} fullWidth variant={'text'}>
-                <Link href={'/auth/sign-up'}>Sign Up</Link>
-              </Button>
-            </div>
-          </form>
-        </Card>
-      </FormProvider>
-    </>
+          </div>
+        </form>
+      </Card>
+    </FormProvider>
   )
 }
