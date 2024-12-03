d3e28906af3f686be049e18e54dedbed6d51d675
krylov72 <qobject1337@gmail.com>
2024-12-03 22:11:31 +0500

    PROJ-42/sign-in refactor: apply common schemas

--- features/SignIn/model/validation.ts
+++ features/SignIn/model/validation.ts
@@ -1,24 +1,7 @@
-import {
-  EMAIL_FORMAT_ERROR_MESSAGE,
-  MAX_CHARACTERS_MESSAGE,
-  MIN_CHARACTERS_MESSAGE,
-  PASSWORD_REQUIREMENTS,
-  REG_EXP_PASSWORD,
-} from '@/shared'
+import { emailSchema, passwordSchema } from '@/entities'
 import { z } from 'zod'
 
 export const signInSchema = z.object({
-  email: z.string().email(EMAIL_FORMAT_ERROR_MESSAGE),
-  password: z
-    .string()
-    .min(6, MIN_CHARACTERS_MESSAGE)
-    .max(20, MAX_CHARACTERS_MESSAGE)
-    .refine(
-      password =>
-        /[0-9]/.test(password) &&
-        /[a-z]/.test(password) &&
-        /[A-Z]/.test(password) &&
-        REG_EXP_PASSWORD.test(password),
-      PASSWORD_REQUIREMENTS
-    ),
+  email: emailSchema,
+  password: passwordSchema,
 })
