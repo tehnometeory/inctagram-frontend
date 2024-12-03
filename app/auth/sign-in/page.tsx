33e49a382916d7927bc359f88fc81faaeea701d1
krylov72 <qobject1337@gmail.com>
2024-12-03 22:08:47 +0500

    PROJ-42/sign-in refactor: move logic to SignUp

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,25 +1,7 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-
 import { SignIn } from '@/features/SignIn'
-import { RootState } from '@/shared'
-import { useRouter } from 'next/navigation'
 
 export default function SignInPage() {
-  const token = useSelector<RootState>(state => state.auth.accessToken)
-  const router = useRouter()
-
-  useEffect(() => {
-    if (token) {
-      router.push('/home')
-    }
-  }, [token, router])
-
-  return (
-    <>
-      <SignIn />
-    </>
-  )
+  return <SignIn />
 }
