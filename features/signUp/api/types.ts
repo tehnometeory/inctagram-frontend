21049e9f19472e4635290d59236df9b7d4bd579a
Yan Turnt <bx6xbchosen1@gmail.com>
2024-12-04 12:48:34 +0500

    refactorProject refactor: add space between types

--- features/signUp/api/types.ts
+++ features/signUp/api/types.ts
@@ -1,6 +1,7 @@
 import { ErrorsMessagesResponse } from '@/shared'
 
 export type RegistrationResponse = ErrorsMessagesResponse | void
+
 export type RegistrationBody = {
   email: string
   password: string
