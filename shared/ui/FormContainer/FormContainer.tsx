8389aa00476ccb82deefbf4749ff7d198b417b2d
Rambo <sandstorm0819@gmail.com>
2024-12-07 12:02:56 +0300

    PROJ-62_formContainer feat: add title to component

--- shared/ui/FormContainer/FormContainer.tsx
+++ shared/ui/FormContainer/FormContainer.tsx
@@ -5,12 +5,15 @@ import clsx from 'clsx'
 
 import s from './FormContainer.module.scss'
 
-type Props = ComponentPropsWithoutRef<'div'>
+type Props = { title?: string } & ComponentPropsWithoutRef<'div'>
 
-export const FormContainer = ({ children, className }: Props) => {
+export const FormContainer = ({ children, className, title, ...props }: Props) => {
   return (
-    <div className={clsx(s.formContainer, className)}>
-      <Card className={s.card}>{children}</Card>
+    <div className={clsx(s.formContainer, className)} {...props}>
+      <Card className={s.card}>
+        <h1 className={s.title}>{title}</h1>
+        <div>{children}</div>
+      </Card>
     </div>
   )
 }
