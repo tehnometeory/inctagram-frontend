1386115351bd59951a0f262cde32fe894be39f0a
Mary-No <mascha.novik@gmail.com>
2024-12-07 14:17:52 +0100

    PROJ-37 fix: fix ExpiredEmailLink, PasswordRecovery and ExpiredEmailLinkForm

--- features/expiredEmailLink/ui/ExpiredEmailLink.tsx
+++ features/expiredEmailLink/ui/ExpiredEmailLink.tsx
@@ -1,29 +1,73 @@
 'use client'
 
-import { UseFormSetError } from 'react-hook-form'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
+import { formWithEmailSchema } from '@/entities'
 import {
+  ErrorsMessagesResponse,
   ExpiredEmailLinkForm,
-  ResendConfirmationCodeArgs,
-  handleRecoveryAndEmailLinkErrors,
+  handleNetworkError,
+  handleServerError,
   useAppDispatch,
 } from '@/shared'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { useResendConfirmationCodeMutation } from '..'
 
+type FormValues = {
+  email: string
+}
+
 export const ExpiredEmailLink = () => {
+  const [showModal, setShowModal] = useState(false)
+  const { control, handleSubmit, reset, setError, watch } = useForm<FormValues>({
+    defaultValues: { email: '' },
+    resolver: zodResolver(formWithEmailSchema),
+  })
+
   const [resendConfirmationCode, { isLoading }] = useResendConfirmationCodeMutation()
 
   const dispatch = useAppDispatch()
 
-  const onSubmit = async (
-    data: ResendConfirmationCodeArgs,
-    setError: UseFormSetError<ResendConfirmationCodeArgs>
-  ) => {
+  const onSubmit = handleSubmit(async data => {
     const response = await resendConfirmationCode(data)
 
-    return handleRecoveryAndEmailLinkErrors(response, setError, dispatch)
+    if (response.error) {
+      if ('status' in response.error && response.error.status === 500) {
+        handleServerError(dispatch)
+      } else if ('status' in response.error && response.error.status === 'FETCH_ERROR') {
+        handleNetworkError(dispatch)
+      } else if ('data' in response.error) {
+        const errorMessage = (response.error.data as ErrorsMessagesResponse).errorsMessages[0]
+          .message
+
+        if (errorMessage === 'User not found') {
+          setError('email', {
+            message:
+              'User with this email are not registered in system or user email already confirmed',
+          })
+        }
+      }
+    } else {
+      setShowModal(true)
+    }
+  })
+  const email = watch('email')
+  const handleCloseShowModal = () => {
+    reset()
+    setShowModal(false)
   }
 
-  return <ExpiredEmailLinkForm isDisabled={isLoading} onSubmit={onSubmit} />
+  return (
+    <ExpiredEmailLinkForm
+      control={control}
+      email={email}
+      handleCloseShowModal={handleCloseShowModal}
+      isDisabled={isLoading}
+      onSubmitHandler={onSubmit}
+      showInput
+      showModal={showModal}
+    />
+  )
 }
