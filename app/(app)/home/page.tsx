a5d95b5a24ca71a2b22f9a080486a7636150137c
Mary-No <mascha.novik@gmail.com>
2024-12-16 19:29:38 +0100

    PROJ-70 refactor: delete test files

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,14 +1,5 @@
 'use client'
 
-import image3 from '@/public/images/216f93efca95a8c000725a6ba9b0d4cd31c39921_high.webp'
-import image2 from '@/public/images/expiredEmail.svg'
-import image from '@/public/images/sign-up.svg'
-import { Carousel } from '@/shared/ui/Carousel'
-
 export default function HomePage() {
-  return (
-    <div>
-      <Carousel images={[image, image2, image3]} type={'Gray'} />
-    </div>
-  )
+  return <div>Home Page</div>
 }
