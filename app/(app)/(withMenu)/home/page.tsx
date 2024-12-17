fef587ec57e087780861973d7bfadcbed504832a
Rambo <sandstorm0819@gmail.com>
2024-12-17 18:55:12 +0300

    PROJ-72_privateLayout feat: add layout with menu and layout without menu in mobile ver

---
+++ app/(app)/(withMenu)/home/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h3>Home Page!!</h3>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem aperiam quaerat
+      soluta quidem est dicta nesciunt, distinctio accusantium eveniet asperiores ab, officiis
+      reiciendis, repellendus quibusdam similique. Voluptas voluptatum quae sed. Lorem ipsum dolor
+      sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero optio illo
+      beatae atque exercitationem necessitatibus aperiam repellendus. Assumenda distinctio tempore
+      quam vel facilis ad repellat accusamus atque explicabo sed, non commodi veritatis vitae ea
+      ipsa dolorum minus? Quos saepe numquam et consequuntur vero iste eveniet repellat. Aut ipsam
+      consectetur blanditiis quo! Hic nam perferendis quasi tenetur quo architecto odit corporis
+      tempora molestias dolor pariatur cum libero accusamus officia itaque assumenda eos, voluptatum
+      quia! Tenetur laborum illo quisquam error sapiente magnam repellendus similique. Quas,
+      cupiditate magni iste animi rerum ab libero deserunt vel excepturi eos voluptatem cum pariatur
+      saepe adipisci id atque doloribus tempore exercitationem praesentium quo voluptate qui officia
+      sunt! Quia provident, reprehenderit perspiciatis non consectetur earum dolorum eos error neque
+      enim cupiditate fugiat, esse veniam. Voluptas ipsam labore ab reprehenderit commodi tempore,
+      saepe minima dolorem expedita, debitis explicabo, aliquid facere! Laboriosam atque dolor
+      repellat illo totam libero quo? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptate fuga quisquam laborum hic libero, ratione sapiente consectetur laudantium eaque,
+      voluptatibus aut quos. Saepe, qui? Rerum libero magnam sint hic, dolore, repellat fuga
+      inventore fugiat excepturi ea deleniti quae harum eligendi quos? Et non harum fugiat quia
+      reiciendis dolores saepe ullam, sint similique excepturi impedit adipisci magni mollitia
+      maiores corporis nesciunt aspernatur cum. Sunt laboriosam placeat consequatur perferendis,
+      incidunt similique architecto omnis, odio, quidem sit eius illo? Quaerat culpa non atque
+      eligendi a consequuntur id iure. Nisi necessitatibus ipsa reprehenderit non quia saepe tenetur
+      eaque impedit error odit, iste, minus aut adipisci enim consectetur ipsum, repellat quos
+      magni. Possimus unde modi vitae dolore veritatis soluta ea illo quo suscipit veniam temporibus
+      autem libero quis, explicabo deserunt dignissimos expedita iure ducimus error, fugiat vero
+      minima. Dicta, ex. Vero fugit, inventore molestiae assumenda architecto quibusdam autem omnis
+      velit minus quidem nesciunt voluptatibus? Impedit atque voluptatibus nulla voluptas quia sit
+      iusto voluptatem cumque, praesentium doloribus omnis aspernatur provident fugit distinctio
+      veniam eligendi error voluptates saepe magni velit nemo? Libero assumenda doloremque, impedit
+      eius totam sed similique magnam ipsam esse fugiat blanditiis id quasi exercitationem expedita
+      molestias, necessitatibus excepturi quod modi distinctio, saepe eum. Harum dignissimos
+      dolores, expedita ut reprehenderit tempore amet perspiciatis sint eaque atque odio ex itaque
+      neque? Omnis adipisci quae quis maxime cumque, optio deleniti ipsum odio tempore? Voluptatum
+      quos odio non repellendus eum dolore impedit, dolor magni ipsum veritatis pariatur aperiam
+      quia saepe ut odit exercitationem fuga eos? Unde, a dolorum possimus rem saepe ipsum magni.
+    </div>
+  )
+}
