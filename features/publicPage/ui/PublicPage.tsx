644e045427c84b1b2beeb375661a972d56b48552
Yan Turnt <bx6xbchosen1@gmail.com>
2025-03-06 20:13:47 +0500

    fix: removed suspense

--- features/publicPage/ui/PublicPage.tsx
+++ features/publicPage/ui/PublicPage.tsx
@@ -1,20 +1,14 @@
-import { Suspense } from 'react'
-
 import s from './PublicPage.module.scss'
 
-import { Posts, PostsSkeleton } from './Posts'
-import { RegisteredUsers, RegisteredUsersSkeleton } from './RegisteredUsers'
+import { Posts } from './Posts'
+import { RegisteredUsers } from './RegisteredUsers'
 
 export const PublicPage = () => {
   return (
     <div className={s.publicPage}>
-      <Suspense fallback={<RegisteredUsersSkeleton />}>
-        <RegisteredUsers />
-      </Suspense>
+      <RegisteredUsers />
 
-      <Suspense fallback={<PostsSkeleton />}>
-        <Posts />
-      </Suspense>
+      <Posts />
     </div>
   )
 }
